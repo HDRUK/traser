3fdffa19cc82e320c006206c4defa1680b4ceb66
Calum Macdonald <calmacx@gmail.com>
2023-07-03 11:34:27 +0100

    test caching data

--- src/routes/translate.js
+++ src/routes/translate.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const jsonata = require('jsonata');
+const fs = require('fs');
 
 const router = express.Router();
 
-const source = {
-    identifier: '<id>',
-    summary:{
-	doiName: '<doi>',
-	title: '<title>',
-	abstract: '<ab>'
+
+// Path to the template file
+const templatePath = './src/templates/test.jsonata';
+let templateData;
+fs.readFile(templatePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Failed to load the JSONata template:', err);
+        return;
     }
-};
+    templateData = data;
+});
 
 
 router.get('/', async (req, res) => {
-    const expression = jsonata('{"identifier": [identifier, summary.doiName], "name": summary.title, "description": summary.abstract }');
-    const result = await expression.evaluate(source); 
+    const expression = jsonata(templateData);
+    const source = {
+        identifier: '<id>',
+        summary: {
+            doiName: '<doi>',
+            title: '<title>',
+            abstract: '<ab>',
+        }
+    };
+    const result = await expression.evaluate(source);
     res.send(result);
+
 });
 
 module.exports = router;
