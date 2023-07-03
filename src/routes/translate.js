d0719a52f3999590a533b3d7483dea84c70913e1
Calum Macdonald <calmacx@gmail.com>
2023-07-03 13:43:57 +0100

    get translate function working

--- src/routes/translate.js
+++ src/routes/translate.js
@@ -7,18 +7,46 @@ const router = express.Router();
 
 // Path to the template file
 const templatePath = './src/templates/test.jsonata';
-let templateData;
+let templateData = {GDMv1:{}};
 fs.readFile(templatePath, 'utf8', (err, data) => {
     if (err) {
         console.error('Failed to load the JSONata template:', err);
         return;
     }
-    templateData = data;
+    templateData['test'] = data;
+});
+
+fs.readFile('./src/templates/GDMv1/HDRUKv211.jsonata', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Failed to load the JSONata template:', err);
+        return;
+    }
+    templateData.GDMv1.HDRUKv211 = data;
 });
 
 
-router.get('/', async (req, res) => {
-    const expression = jsonata(templateData);
+router.post('/', async (req, res) => {
+    const body = req.body;
+
+    const source = {
+        input: body.metadata,
+        extra: body.extra
+    }
+
+    try{
+        const expression = jsonata(templateData.GDMv1.HDRUKv211);
+        const result = await expression.evaluate(source);
+        res.send(result);
+    }
+    catch (err) { 
+        res.send(err)
+    }
+
+});
+
+
+router.get('/test', async (req, res) => {
+    const expression = jsonata(templateData['test']);
     const source = {
         identifier: '<id>',
         summary: {
@@ -29,7 +57,10 @@ router.get('/', async (req, res) => {
     };
     const result = await expression.evaluate(source);
     res.send(result);
-
 });
+
+
+//
+
 
 module.exports = router;
