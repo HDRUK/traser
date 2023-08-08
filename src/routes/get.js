ad3f2eed995e3cce7263b998000df7b9fa3e1d05
Calum Macdonald <calmacx@gmail.com>
2023-08-08 16:05:23 +0100

    implementing get/map with validation

--- src/routes/get.js
+++ src/routes/get.js
@@ -2,49 +2,73 @@ const express = require('express');
 const jsonata = require('jsonata');
 const cacheHandler = require('../middleware/cacheHandler');
 
+const { query, validationResult, matchedData } = require('express-validator');
+
 const router = express.Router();
 
 /**
  * @swagger
- * /get:
+ * /get/map:
  *   get:
- *     summary: Retrieve a list of JSONPlaceholder users
- *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
-*/
-router.get('/', async (req, res) => {
+ *     summary: Retrieve a template or mapping file
+ *     description: Retrieve a template or mapping file from the cacheHandler.
+ *     parameters:
+ *       - in: query
+ *         name: to
+ *         schema:
+ *           type: string
+ *         description: The output schema name.
+ *       - in: query
+ *         name: from
+ *         schema:
+ *           type: string
+ *         description: The input schema name.
+ *     responses:
+ *       200:
+ *         description: Template or mapping retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 mapping:
+ *                   type: object
+ *                   description: The retrieved template or mapping.
+ */
+router.get('/map',
+    [
+        query('to').notEmpty().escape(),
+        query('from').notEmpty().escape(),
+    ],
+    async (req, res) => {
 
-    const queryString = req.query;
-    
-    // Define the required query parameters
-    const requieredQueries = ['to','from'];
-    // check that the queryString contains all these values
-    const containsAllValues = requieredQueries.every(
-        (v) => Object.keys(queryString).includes(v)
-    );
-    if (!containsAllValues){ 
-        return res.status(400).json({ 
-            error: 'Invalid query parameters.',
-            required: requieredQueries,
-            missing: requieredQueries.filter(
-                (v) => !Object.keys(queryString).includes(v)
-            )
-        });
-    }
-    const output_model_name = queryString['to'];
-    const input_model_name = queryString['from'];
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+	    return res.status(400).json({ 
+		message: 'Invalid query parameters.',
+		errors: result.array()
+	    });
+	}
 
-    const template = cacheHandler.getTemplate(output_model_name,input_model_name);
-    if (template == null){
-        return res.status(400).json({ 
-            error: 'Template file is null!', 
-            details: `Could not retrieve the template for output:${output_model_name} input:${input_model_name}`
-        });
-    }
+	const queryString = matchedData(req);
+	const output_model_name = queryString['to'];
+	const input_model_name = queryString['from'];
+	
+	const template = cacheHandler.getTemplate(output_model_name,input_model_name);
+	if (template == null){
+	    return res.status(400).json({ 
+		error: 'Template file is null!', 
+		details: `Could not retrieve the template for output:${output_model_name} input:${input_model_name}`
+	    });
+	}
+	
+	res.send({
+	    "from":input_model_name,
+	    "to":output_model_name,
+	    "translation_map":template
+	});
 
-    //const expression = jsonata(template);
-    res.send({"template":template});//"expression":expression})
-
-});
+    });
 
 
 
