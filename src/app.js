a6d642ed6e7d794bc8ac2d19f4eeb371657c5dce
Calum Macdonald <calmacx@gmail.com>
2023-08-08 13:53:00 +0100

    cleaned up app.js

--- src/app.js
+++ src/app.js
@@ -1,40 +1,54 @@
 const express = require('express');
 
+//documentation 
 const swaggerJsdoc = require("swagger-jsdoc"),
       swaggerUi = require("swagger-ui-express");
 
-const swaggerDocument = require('../swagger.json');
-
-
+//recommendations for express.js
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const path = require('path');
 
+//load API routes
 const indexRouter = require('./routes/index');
 const translateRouter = require('./routes/translate');
 const getRouter = require('./routes/get');
 const validateRouter = require('./routes/validate');
-//const visualiseRouter = require('./routes/visualiser');
+
+//load middleware
 const errorHandler = require('./middleware/errorHandler');
 const cacheHandler = require('./middleware/cacheHandler');
 
+//create the app
 const app = express();
 
-cacheHandler.load();
+//call the cacheHandler to load all files
+// - this will be revisted when we implement file caching properly
+// - i.e. if files are changed, will have to call .load() again
+// Notes:
+// - can we deploy is prod so that the app watches for file changes?
+//   (in a similar way to how it works in dev localy)
+cacheHandler.loadData();
 
-
+//additional setups
 app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
-app.use(logger('dev'));
+app.use(logger('dev')); //may want to remove/change this for production (?)
+//setup express.js
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); //not sure if this is needed?
 
+// Temporary loading up swagger API for auto documentations
+// Notes:
+// - could/should switch to a loading configuration from json
+// - const swaggerDocument = require('../swagger.json');
+// - could/should put this in a seperate file (not in app.js)
 const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
-    title: 'Express API for JSONPlaceholder',
+    title: 'Express API for metadata JSON translation service',
     version: '1.0.0',
   },
 };
@@ -46,12 +60,12 @@ const swaggerSpec = swaggerJsdoc(options);
 app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 //app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
-
+//register all the routes
 app.use('/', indexRouter);
 app.use('/translate', translateRouter);
 app.use('/get', getRouter);
 app.use('/validate', validateRouter);
-//app.use('/visualise', visualiseRouter);
+
 // Serve static files from the "public" folder
 app.use('/files',express.static(path.join(__dirname,'public')));
 
@@ -62,7 +76,5 @@ app.use((req, res, next) => {
 
 // pass any unhandled errors to the error handler
 app.use(errorHandler);
-
-
 
 module.exports = app;
