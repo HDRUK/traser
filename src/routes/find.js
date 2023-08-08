aa04bb06d165329b3dbe923e85b05ae56d901a53
Calum Macdonald <calmacx@gmail.com>
2023-08-08 17:17:45 +0100

    forgot to commit file

---
+++ src/routes/find.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const cacheHandler = require('../middleware/cacheHandler');
+const { body, validationResult } = require('express-validator');
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /find:
+ *   post:
+ *     summary: Validate posted metadata against available schemas
+ *     description: Validate posted metadata against available schemas in the cacheHandler.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               metadata:
+ *                 type: object
+ *                 description: The metadata object to validate.
+ *     responses:
+ *       200:
+ *         description: Schema validation results.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               description: List of schema validation results.
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                     description: The name of the schema.
+ *                   matches:
+ *                     type: boolean
+ *                     description: Indicates whether the metadata matches the schema.
+ *       400:
+ *         description: Bad request due to invalid content type or validation errors.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   description: Array of validation errors.
+ */
+router.post(
+    '/',
+    body().custom((value, { req }) => {
+        if (!req.is('application/json')) {
+            throw new Error('Invalid content type. Expected JSON.');
+        }
+        return true;
+    }),
+    async (req, res) => {
+	
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+	}
+
+	//retrieve the posted data 
+	const metadata = req.body;
+	
+	
+	const schemas = cacheHandler.getSchemas();
+	
+	const result = Object.keys(schemas).map(name => {
+	    
+	    //for the schema to check, retrieve the validator
+	    const input_validator =  schemas[name].validator;
+	    //check if the metadata is valid 
+	    let isValid = input_validator(metadata);
+	    
+	    const retval = {"name":name,"matches":isValid};
+	    
+	    return retval;
+	});
+	res.send(result);
+	
+    });
+
+module.exports = router;
