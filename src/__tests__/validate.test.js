aedc932aeb9b0bc28cbbc85457bb71e2f71cc3ea
Calum Macdonald <calmacx@gmail.com>
2023-09-11 15:34:42 +0100

    adding in more testing

---
+++ src/__tests__/validate.test.js
@@ -0,0 +1,69 @@
+const request = require('supertest');
+const app = require('../app');
+
+const fs = require('fs');
+const path = require('path');
+
+const sampleMetadata = {};
+const sampleMetadataDir = path.join(__dirname, './data'); 
+
+fs.readdirSync(sampleMetadataDir).forEach((file) => {
+  if (file.endsWith('.json')) {
+    const fileName = path.parse(file).name;
+    const filePath = path.join(sampleMetadataDir, file);
+    sampleMetadata[fileName] = require(filePath);
+  }
+});
+
+const validate = async (metadata,modelName) => {
+    const requestBody = {
+	metadata: metadata
+    }
+
+    const response = await request(app)
+	  .post('/validate')
+	  .query({ model_name: modelName})
+	  .send(requestBody);
+    
+    return response;
+};
+
+/*
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+beforeAll((done) => {
+    // need a better way of doing this...
+    // waiting for the app to load templates before trying to run tests 
+    // app.emit() ?
+    delay(1000).then(() => {
+	console.log('ran after 1 second1 passed');
+	done();
+    });
+ });
+*/
+
+describe('POST /validate', () => {
+    describe('POST /validate?model_name=gdmv1', () => {
+	it('should return 200 if gdmv1 can be validated', async () => {
+	    const response = await validate(sampleMetadata.gdmv1,
+					    'gdmv1'
+					   );
+	    expect(response.status).toBe(200);
+	});
+    });
+    
+});
+
+describe('POST /validate', () => {
+    describe('POST /validate?model_name=hdrukv211', () => {
+	it('should return 200 if HDRUK 2.1.1 can be validated', async () => {
+	    const response = await validate(sampleMetadata.hdrukv211,
+					    'hdrukv211'
+					   );
+	    expect(response.status).toBe(200);
+	});
+    });
+    
+});
