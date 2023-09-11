80c511c79ac8e1ce7516ef08c7eac90fb45786bf
Calum Macdonald <calmacx@gmail.com>
2023-09-11 14:24:19 +0100

    add testing and eslint configuration

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -48,9 +48,12 @@ describe('app', () => {
 });
 
 
-const translate = async (metadata,inputModel,outputModel,validateInput='1',validateOutput='1') => {
+const translate = async (metadata,inputModel,outputModel,validateInput='1',validateOutput='1',extra=null) => {
     const requestBody = {
 	metadata: metadata
+    }
+    if (extra !==null){
+	requestBody.extra = extra
     };
 
     const response = await request(app)
@@ -66,16 +69,51 @@ describe('POST /translate', () => {
 
     describe('POST /translate?to=schemaorg&from=gdmv1 ', () => {
 	it('should return 200 if gdmv1 metadata translated to schema.org', async () => {
-	    const response = await translate(sampleMetadata.gwdm,'gdmv1','schemaorg');
+	    const response = await translate(sampleMetadata.gdmv1,
+					     'gdmv1',
+					     'schemaorg'
+					    );
 	    expect(response.status).toBe(200);
 	});
     });
 
     describe('POST /translate?to=gdmv1&from=schemaorg ', () => {
 	it('should return 200 if schema.org metadata translated to GDMV1', async () => {
-	    const response = await translate(sampleMetadata.schemaorg,'schemaorg','gdmv1');
+	    const response = await translate(sampleMetadata.schemaorg,
+					     'schemaorg',
+					     'gdmv1'
+					    );
 	    expect(response.status).toBe(200);
 	});
     });
+
+    describe('POST /translate?to=gdmv1&from=hdrukv211', () => {
+	it('should return 200 if HDRUK 2.1.1 metadata translated to GDMV1', async () => {
+	    const response = await translate(sampleMetadata.hdrukv211,
+					     'hdrukv211',
+					     'gdmv1',
+					     '1',
+					     '1',
+					     sampleMetadata.extra_hdrukv211
+					    );
+	    console.log(response.text);
+	    expect(response.status).toBe(200);
+	});
+    });
+
+    describe('POST /translate?to=hdrukv211&from=gdmv1', () => {
+	it('should return 200 if  metadata GDMV1 translated to HDRUK 2.1.1', async () => {
+	    const response = await translate(sampleMetadata.gdmv1,
+					     'gdmv1',
+					     'hdrukv211',
+					     '1',
+					     '1',
+					     sampleMetadata.extra_gdmv1
+					    );
+	    console.log(response.text);
+	    expect(response.status).toBe(200);
+	});
+    });
+
     
 });
